0def4fb3d462f5ed4e2264bec0930a29168a244e
Gesiel Lucas <gesiel.lucas.f@gmail.com>
2023-10-18 20:50:41 -0300

    feat: add breeze and debts model w/ all

---
+++ app/Http/Controllers/DebtsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDebtsRequest;
+use App\Http\Requests\UpdateDebtsRequest;
+use App\Models\Debts;
+
+class DebtsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDebtsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Debts $debts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Debts $debts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDebtsRequest $request, Debts $debts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Debts $debts)
+    {
+        //
+    }
+}
